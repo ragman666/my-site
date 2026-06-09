6009a09730ebfc69e73229e33e5a753531939490
Adam Marflitt <adam_the_incontinent@hotmail.com>
2026-06-09 17:49:11 +0100

    change look of the site and auto increment

--- apilookups.js
+++ apilookups.js
@@ -6,6 +6,8 @@ const sports_teams = [
     { team: 'Yorkshire CC', thesportsdbid: '135763' }
 ];
 
+let count = 1;
+
 let displayall = document.querySelector("#displayallid");
 let sport = document.querySelector("#sportid");
 
@@ -80,32 +82,33 @@ const displayDatayorks=(yorksgame)=>{
     yorksgametime.innerText=`${yorksgame.events[0].dateEventLocal}   ${yorksgame.events[0].strTimeLocal}`
 }
 
-function addto1(teamId) {
+// gather Data from the sportsdb API and add it to the columns on the page. This is done by calling the addtocolumns function with the team id as an argument. The addtocolumns function then fetches the data from the API and calls the pushtobox function with the column id and the game data as arguments. The pushtobox function then creates a new box with the game data and adds it to the column.
+function addtocolumns(teamId) {
+    const columnId = `endof${count}`;
+    count += 1;
+
     fetch(`https://www.thesportsdb.com/api/v1/json/123/eventsnext.php?id=${teamId}`)
     .then(response => response.json())
-    .then(
-        pushtobox)
-    .catch(err => alert('add to 1 failed')); 
+    .then(game => pushtobox(columnId, game))
+    .catch(err => alert('add to 1 failed'));
 }
-const pushtobox=(game)=>{
+// Gather the data from the API and push it to the box. This is done by calling the createnicebox function with the column id and the game data as arguments. The createnicebox function then creates a new box with the game data and adds it to the column.
+const pushtobox=(columnId, game)=>{
 
     const p1 = `${game.events[0].strEvent}`
     const p2 = `${game.events[0].strLeague}`
     const p3 = `${game.events[0].dateEventLocal}   ${game.events[0].strTimeLocal}`
 
-    createnicebox("endof1",p1,p2,p3)
+    createnicebox(columnId, p1, p2, p3)
+
 }
 
-
-
-
+// Create a nice box to display the data in the page
 function createnicebox(column,p1text,p2text,p3text) {
-  // create a new div element
+
   const newDiv = document.createElement("div")
   newDiv.classList.add("nicebox");
 
-  // and give it some content
-  //const newContent = document.createTextNode("Hi there and greetings!");
   const p1 = document.createElement("p")
   p1.innerText = p1text;
   const p2 = document.createElement("p")
@@ -113,33 +116,21 @@ function createnicebox(column,p1text,p2text,p3text) {
   const p3 = document.createElement("p")
   p3.innerText = p3text;
 
-  // add the text node to the newly created div
   newDiv.appendChild(p1);
   newDiv.appendChild(p2);
   newDiv.appendChild(p3);
-
-
-  // add the newly created element and its content into the DOM
+  
   const currentDiv = document.getElementById(`${column}`);
   currentDiv.parentNode.insertBefore(newDiv, currentDiv);
 
   const newbreak = document.createElement("br")
+  const newbreak2 = document.createElement("br")
+  const newbreak3 = document.createElement("br")
   currentDiv.parentNode.insertBefore(newbreak, currentDiv);
-  currentDiv.parentNode.insertBefore(newbreak, currentDiv);
-  currentDiv.parentNode.insertBefore(newbreak, currentDiv);
+  currentDiv.parentNode.insertBefore(newbreak2, currentDiv);
+  currentDiv.parentNode.insertBefore(newbreak3, currentDiv);
 
-  //newDiv.appendChild(document.createElement("br"));
-  //newDiv.appendChild(document.createElement("br"));
-  //newDiv.appendChild(document.createElement("br"));
 }
-
-
-//            <div class="nicebox">
-//                <p id="lufcnextgameid">-----</p>
-//                <p id="lufcnextgameleagueid">-----</p>
-//                <p id="lufcnextgametimeid">-----</p>
-//            </div>
-//                <br>
 
 
 
@@ -150,8 +141,8 @@ function init() {
 //    rhinosnextgame()
 //    yorksnextgame()
 //    createnicebox("endof1","p1","p2","p3")
-    addto1("135763")
-    addto1("133635")
+    addtocolumns("135763")
+    addtocolumns("133635")
 }
 
 document.addEventListener('DOMContentLoaded', init);
