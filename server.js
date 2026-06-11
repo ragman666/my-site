b6ce51de7938900f30023ea2233e26464c417cad
Adam Marflitt <adam_the_incontinent@hotmail.com>
2026-06-11 12:01:31 +0100

    move to node.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+
+// Serve all static files (HTML, CSS, JS, images, etc.) from the 'public' folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html on the root route
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+ 
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
